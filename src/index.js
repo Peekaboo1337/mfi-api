37770c951d8b3beb36ed0e73d133174da35e32a9
Erek Röös <erek@localhost.localdomain>
2017-11-27 20:08:54 +0100

    :ambulance:

--- src/index.js
+++ src/index.js
@@ -21,10 +21,29 @@ const mfiLogin = (username, password, ip) => {
     })
 
     conn.connect({
-      host: '',
+      host: ip,
       port: 22,
       username: 'ubnt',
       password: 'ubnt',
+      algorithms: {
+        kex: ['diffie-hellman-group1-sha1'],
+        cipher: [
+          '3des-cbc',
+          'blowfish-cbc',
+          'cast128-cbc',
+          'arcfour',
+          'arcfour128',
+          'arcfour256',
+          'aes128-cbc',
+          'aes192-cbc',
+          'aes256-cbc',
+          'aes128-ctr',
+          'aes192-ctr',
+          'aes256-ctr',
+          'aes128-gcm@openssh.com',
+          'aes256-gcm@openssh.com',
+        ],
+      },
     })
   })
 }
@@ -35,12 +54,25 @@ const mfiLogout = ip => {
 
 const setSensor = (sensorId, output, ip) => {
   return new Promise((resolve, reject) => {
-    connections[ip].exec(`/usr/bin/echo ${output} > /proc/power/output${sensorId}`, (code, signal) => {
-      if (code === 0)
-        return resolve({ code, signal })
+    connections[ip].exec(`/usr/bin/echo ${output} > /proc/power/output${sensorId}`, (err, stream) => {
+      if (err)
+        debug(err)
 
-      else
-        return reject({ code, signal })
+      stream.on('close', (code, signal) => {
+        debug(code)
+        debug(signal)
+
+        debug('Execution finished')
+        return resolve()
+      })
+
+      stream.on('data', data => {
+        debug('STDOUT: ' + data)
+      })
+
+      stream.stderr.on('data', data => {
+        debug('STDERR: ' + data)
+      })
     })
   })
 }
