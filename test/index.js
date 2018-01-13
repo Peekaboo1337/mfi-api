5084777805ddbe06f7ab51a0c8710ce202ff0da8
Erek Röös <erek.roeoes@gmail.com>
2018-01-13 17:28:23 +0100

    New test

--- test/index.js
+++ test/index.js
@@ -2,19 +2,19 @@
 const ms = require('ms')
 const { mfiLogin, setSensor, mfiLogout } = require('../src')
 
-const main = async () => {
+const __main__ = async () => {
   try {
-    await mfiLogin('ubnt', 'ubnt', '192.168.178.32')
-    await setSensor(1, 1, '192.168.178.32')
-    setTimeout(async () => await setSensor(1, 0, '192.168.178.32'), ms('10s'))
-    setTimeout(async () => await setSensor(1, 1, '192.168.178.32'), ms('20s'))
-    setTimeout(async () => await setSensor(1, 0, '192.168.178.32'), ms('30s'))
-    setTimeout(async () => await setSensor(1, 1, '192.168.178.32'), ms('40s'))
-    setTimeout(async () => await setSensor(1, 0, '192.168.178.32'), ms('50s'))
-    setTimeout(async () => await mfiLogout('192.168.178.32'), ms('1m'))
+    await mfiLogin('ubnt', 'ubnt', '192.168.2.106')
+    await setSensor(1, 1, '192.168.2.106')
+    setTimeout(async () => setSensor(1, 0, '192.168.2.106'), ms('10s'))
+    setTimeout(async () => setSensor(1, 1, '192.168.2.106'), ms('20s'))
+    setTimeout(async () => setSensor(1, 0, '192.168.2.106'), ms('30s'))
+    setTimeout(async () => setSensor(1, 1, '192.168.2.106'), ms('40s'))
+    setTimeout(async () => setSensor(1, 0, '192.168.2.106'), ms('50s'))
+    setTimeout(async () => mfiLogout('192.168.2.106'), ms('1m'))
   } catch (err) {
-    // console.log(err)
+    console.log(err)
   }
 }
 
-main()
+__main__()
